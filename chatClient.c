31241c6aac04e9036a8352c51325aa1b9f09a8fb
Haider Naeem <39599286+HaiderNaeem@users.noreply.github.com>
2020-04-11 15:56:20 -0400

    Update chatClient.c

--- chatClient.c
+++ chatClient.c
@@ -21,38 +21,38 @@ int main (int argc, char *argv[]){
 	char buffer[255];
 
 	if(argc < 3){
-	fprintf(stderr, "usage %s hostname port\n", argv[0]);
+	fprintf(stderr, "usage %s hostname/ip port.\n", argv[0]);
 	exit(0);
 }
 	portno = atoi(argv[2]);
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0){
-	error("Error opening socket");
+	error("Error opening socket.\n");
 }
 
 	server = gethostbyname(argv[1]);
 	if(server == NULL){
-	fprintf(stderr, "Error, host not found");
+	fprintf(stderr, "Error, host not found.\n");
 }
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, 		server->h_length);
+	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
 	serv_addr.sin_port = htons(portno);
-	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 	0){
-	error("Connection Failed\n");
+	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+	error("Connection Failed.\n");
 }
 	while(1){
 	bzero(buffer, 255);
 	fgets(buffer, 255, stdin);
 	n = write(sockfd, buffer, strlen(buffer));
 	if(n < 0){
-	error("Error on Writing.");
+	error("Error on Sending Msg.\n");
 }
 
 	bzero(buffer, 255);
 	n = read(sockfd, buffer, 255);
 	if(n < 0){
-	error("Error on Reading.");
+	error("Error on Recieving Msg.\n");
 }
 	printf("Server: %s", buffer);
 	int i = strncmp("Bye", buffer, 3);
@@ -62,6 +62,7 @@ int main (int argc, char *argv[]){
 
 }// end while
 	close(sockfd);
+	printf("Disconected.\n")
 	return 0;
 
 }// end main
